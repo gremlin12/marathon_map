04f3a54e374b83f45a9dd6a68b25e85f43314f41
gremlin12 <zomorabbit@gmail.com>
2015-02-12 06:41:12 -0500

    Add currentList variable

--- js/app4.js
+++ js/app4.js
@@ -1,4 +1,6 @@
-var places = [    
+var model = {
+
+  places : [    
      {
         name : 'Marathon Library',
         lat: 24.710633,
@@ -6,16 +8,26 @@ var places = [
 
      },
      {
+        name : 'Marathon Community Park',
+        lat : 24.711631,
+        long: -81.089487
+     }
+],
+
+  beaches  : [
+     {
         name : 'Sombrero Beach',
         lat: 24.691533,
         long: -81.086107
      },
      {
-        name : 'Marathon Community Park',
-        lat : 24.711631,
-        long: -81.089487
-     }
-];
+        name : 'Coco Plum Beach',
+        lat : 24.730240,
+        long : -81.001592
+     }   
+
+]
+};
 
 function Point(name, lat, long) {
     this.name = ko.observable(name);
@@ -31,9 +43,10 @@ function Point(name, lat, long) {
         return function() {
           infowindow.setContent(name);
           infowindow.open(map, marker);
-        }
+        };
     })(marker));
 }
+
 
 var map = new google.maps.Map(document.getElementById('map-canvas'), {
     zoom: 13,
@@ -42,13 +55,15 @@ var map = new google.maps.Map(document.getElementById('map-canvas'), {
 
 var infowindow = new google.maps.InfoWindow();
 
+var currentList = model.places;
 
 var viewModel = function() {
     var self = this;
+
     points = ko.observableArray([]);
-    for (place in places) {
-        points.push(new Point(places[place].name, places[place].lat, places[place].long));
-    }
+    for (var place in currentList) {
+        points.push(new Point(currentList[place].name, currentList[place].lat, currentList[place].long));
+    }  
 };
 
 ko.applyBindings(viewModel());
