a2fd9ffe3f230351134ea2eb4bb72d8573f9e610
gremlin12 <zomorabbit@gmail.com>
2015-03-02 17:17:07 -0500

    Add js comments

--- js/app5.js
+++ js/app5.js
@@ -1,3 +1,8 @@
+/* Model */
+
+//   Custom locations can be added independently of Google Places by adding data manually
+//   to the model. The custom category "beaches" has been added as an example.
+
 var model = [    
      {
         name : 'Sombrero Beach',
@@ -27,7 +32,8 @@ var model = [
 /* Google Map View */
 
 
-// Set up Google Map 
+// Set up Google Map. To customize for a different city or region, change 
+// the lat/long coordinates.
 
 var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 13,
@@ -50,9 +56,9 @@ view = {
     // Smaller mobile devices will use icons for menu tabs. 
 
     init : function() {
-        var categories = ['lodging', 'culture', 'recreation', 'government', 'services'];
+        var categories = ['hospitality', 'culture', 'recreation', 'government', 'services'];
         var icons = ['&#9789;', '&#9774;', '&#9786;', '&#10026;', '&#36;'];
-        var labels = ['Eating & Sleeping', 'Culture', 'Recreation', 'Government', 'Services']
+        var labels = ['Hospitality', 'Culture', 'Recreation', 'Government', 'Services'];
         for (i=0; i < categories.length; i++) {
             if ($('#map-canvas').width() > 525)  {  
                 $( '#' + categories[i] ).replaceWith( '<a href="#" id=' + categories[i] +'>' + labels[i] + '</a>' );
@@ -87,8 +93,8 @@ view = {
         }
 
         /* This function collects and saves the data needed to create a marker for each place.
-           Later, this data will be used by the addMarkers() function to place markers on the map.
-           Details of each place are saved as variables. */
+           Later, the data will be used by the addMarkers() function to place markers on the map.
+           Details of each place are saved to the model and to the locations array. */
 
         function createMarker(place) {
             var name = place.name;
@@ -129,8 +135,11 @@ view = {
         }  
     },
 
-    // Add a marker to the map for each place, using data prepared by 
-    // the createMarker() function above. 
+    /* Add a marker to the map for each place, using data prepared by 
+       the createMarker() function above. The addMarkers() function
+       is triggered each time a new Location object is added to the 
+       locations array. */
+
     addMarkers : function(name,lat,long,cat,address,imgUrl,placeid) {
         marker = new google.maps.Marker({
             position: new google.maps.LatLng(lat, long),
@@ -153,7 +162,13 @@ view = {
         // Each marker's data is saved as a json in the markers array.    
         markers.push(marker);
     },
-
+    
+    /* The getPlaceDetails() function is triggered when the user clicks on a location name
+       rendered in the results area. It sends an additional request to Google for more
+       detailed information about the location. The phone number, website, and Google place_id 
+       are collected and saved from the returned results, and these data are passed on to the 
+       openInfoWindow() function in the viewModel.  */
+       
     getPlaceDetails: function (placeid) {
         var phoneNumber ='';
         var website = '';
@@ -173,8 +188,16 @@ view = {
            
         }
     },
+    
+    /* The next two functions are triggered when a user clicks on a map-marker. The function
+       sortClickedMarkers() checks whether the clicked marker represents a custom location  
+       from the model or a Google Places location delivered by Google's API. If the marker 
+       belongs to a Google location, the getClickedPlaceDetails() is called. If it belongs 
+       to a custom location, the phone number and website are collected from the model and 
+       sent straight to the openInfoWindow() function. */
 
     sortClickedMarkers : function (placeid) {
+        // A Google place-id must be 27 characters in length.
         if (ko.utils.unwrapObservable(placeid).length > 6) {
             view.getClickedPlaceDetails(placeid);
         }
@@ -188,6 +211,10 @@ view = {
             } 
         } 
     }, 
+    
+    /* The getClickedPlaceDetails() function sends a Place Details request to Google
+       when a marker is clicked. The returned results (phone number, place-id, website) are
+       used as arguments when the openInfoWindow() function is called. */
 
     getClickedPlaceDetails : function (placeid) {
         var phoneNumber ='';
@@ -215,10 +242,18 @@ view = {
 
 var viewModel = function() {
     var self = this;
+
+    // The locations array holds data collected from a Google Places search
+    // (see the createMarker() function above). 
+
     this.locations = ko.observableArray([]);
+
+    // The query string is created when a user submits input from the search box.
+
     this.query = ko.observable('');
     
-    
+    // Create an object for each location found by Google Places search. 
+
     this.Location = function (name, lat, long, cat, address, imgUrl,placeid) {
         this.name = ko.observable(name);
         this.lat = lat;
@@ -227,8 +262,13 @@ var viewModel = function() {
         this.imgUrl = ko.observable(imgUrl);
         this.placeid = ko.observable(placeid);
 
+        // Call the addMarkers() function to add a marker to the map.
         view.addMarkers(name, lat, long, cat, address, imgUrl,placeid);
     };
+
+    /*Remove the markers from the map and clear the results area. The emptyLocations()
+      function is triggered when a new category tab in the menu is clicked, or when the
+      map is reset by the user. */
 
     this.emptyLocations = function() {
          self.recenterMap();
@@ -239,6 +279,11 @@ var viewModel = function() {
          self.locations.removeAll();
     };
     
+    /* The getPlaces() function is called when a user clicks on a custom category tab 
+       (such as "beaches") in the navigation menu. It collects place details from
+       the model and pushes a new Location object into the locations array, which
+       results in a new marker being added to the map. */
+
     this.getPlaces = function(category){
         for (var place in model) {
             for(i=0; i < model[place].cat.length; i++) {
@@ -249,6 +294,13 @@ var viewModel = function() {
         }    
     };
 
+    /* The sortPlaces() function is triggered when the user clicks on a location name
+       rendered in the results area. It checks whether the place-id belongs to a custom
+       location or to a Google Places location. In the latter case, a Place Details
+       search request is sent to Google. Otherwise, the necessary details are retrieved
+       directly from the model and sent to openInfoWindow().  */
+
+    
     this.sortPlaces = function(placeid) {
         if (ko.utils.unwrapObservable(placeid).length > 6) {
             view.getPlaceDetails(placeid);
@@ -264,18 +316,25 @@ var viewModel = function() {
             }        
         }
     };
+    
+    /* Recenter the map. This function is triggered when a new category tab on the menu
+       is selected, or when the recenter-map button is clicked. */
 
     this.recenterMap = function() {
         map.setOptions(
             {
-                'center': {lat: 24.723009, lng: -81.038884},
+                'center' : map.center,
                 'zoom' : 12
             });       
         self.closeInfoWindow();
     };
-
-
-    this.searchPlaces = function(query) {
+ 
+    /* The searchBox() function handles user input from the search box. It scans the
+       model for a location name matching the query string. Since all locations rendered
+       on the map are saved in the model, the user can search for a specific location 
+       even after its marker has been cleared from the map.*/
+    
+    this.searchBox = function(query) {
         var search = this.query().toLowerCase();
         for (place in model) {
             if (model[place].name.toLowerCase() === search) {
@@ -287,7 +346,8 @@ var viewModel = function() {
         this.recenterMap();
 
     }; 
-
+    
+    // Close the infoWindow.
 
     this.closeInfoWindow = function() {
         if (infoWindowIsOpen === true) {
@@ -295,30 +355,38 @@ var viewModel = function() {
         }
         infoWindowIsOpen = false;
     };
+  
+    /* The openInfoWindow() function is the last in a series of functions triggered when
+       the user clicks a marker on the map or a location name appearing in the results area.
+       It takes the place details collected by preceeding functions  and turns them into 
+       a content string for the infoWindow.  */
 
     this.openInfoWindow = function(phoneNumber, placeid, website) {
-
-        for (place in model) {
+        // Search the model for a matching place-id. 
+        for (place in model) {            
+            // If a match is found, collect details from the model and save as variables.
             if (model[place].placeid === placeid) {
                 var currentLat = model[place].lat;
                 var currentLong = model[place].long;
                 var currentName = model[place].name;
                 var currentAddress = model[place].address;
+                var currentPlaceId = model[place].placeid;
 
-                //if (window.matchMedia("(min-width: 400px)").matches) {
+                // Check for a mobile display. If the screen is too small, leave the image out.
                 if ($('#map-canvas').height() >= 350) {  
                     var currentImage = model[place].imgUrl;
                 } else {
                     currentImage = '';
-                }
-                
-                var currentPlaceId = model[place].placeid;
+                }           
+
+                // Check if a phone number was returned by Google Places.
                 if (phoneNumber === undefined) {
                     var currentPhone = '';
                 }
                 else {
                     currentPhone = phoneNumber;
                 }
+                // Check if a website address was returned by Google Places
                 if (website === undefined) {
                     var currentWebsite = '';
                 }
@@ -328,6 +396,8 @@ var viewModel = function() {
             }
         }
   
+        // Construct the content string according to what details are available.
+
         if (currentImage === '') {
             if (currentWebsite === '') {
                 var content = '<p class="bold">' + currentName +'</p><p>' + currentAddress + '<br/>' + currentPhone + '</p>'
@@ -343,15 +413,21 @@ var viewModel = function() {
             content = '<p class="bold">' + currentName + '</p><img src="' + currentImage + '""><p>' + currentAddress +'<br/>'+ currentPhone +'<br/><a href="' + currentWebsite + '">Website</a></p>';
         }
         
+        // Set the infoWindow's position, add content, and open it.
+
         infowindow.setPosition({lat: currentLat, lng: currentLong});
         infowindow.setOptions({pixelOffset : new google.maps.Size(0,-35)});
         infowindow.setContent(content);
         infowindow.open(map);
         infoWindowIsOpen = true;
+
+        // Zoom to the corresponding place on the map.
         map.setOptions({
             'zoom' : 15,
             'center' : {lat: currentLat, lng: currentLong}
         });
+
+        // Scroll to top of screen to see the map.
         $('html, body').animate({ scrollTop: 0 }, 'fast');
     };
 
